ab14afdb1b5076ea771a074f84c1a52873f99de2
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-06 23:55:33 -0600

    Add trail, dynamic color/opacity and rework grid
    - add addColorChangeOnHover function
    - rework createGrid function

--- script.js
+++ script.js
@@ -14,13 +14,53 @@ document.addEventListener("DOMContentLoaded", () => {
         for (i = 0; i < squares; i++) {
             const row = document.createElement("div");
             row.classList.add('row');
+            row.setAttribute('opacity', '.1');
             container.appendChild(row);
             for (j = 0; j < squares; j++) {
                 const square = document.createElement('div');
                 square.classList.add('square');
+                square.setAttribute('opacity', '.1');
                 row.appendChild(square);
             }
         }
+        addColorChangeOnHover();
+    }
+
+    // Add pixel trail to squares
+    addColorChangeOnHover = () => {
+        const children = Array.from(container.childNodes);
+
+        children.forEach((v) => {
+            v.addEventListener("mouseover", (e) => {
+                const red = parseInt(Math.random() * 256);
+                const green = parseInt(Math.random() * 256);
+                const blue = parseInt(Math.random() * 256);
+                let opacity = parseFloat(e.target.getAttribute('opacity'));
+
+                let redAttr = e.target.getAttribute('red') || e.target.setAttribute('red', red);
+                let greenAttr = e.target.getAttribute('green') || e.target.setAttribute('green', green);
+                let blueAttr = e.target.getAttribute('blue') || e.target.setAttribute('blue', blue);
+                
+                if (e.target.classList[1] === 'hovered') {
+                    opacity += 0.1;
+                    e.target.setAttribute('opacity', opacity);
+                    console.log(opacity);
+                    e.target.style.backgroundColor = `rgba(
+                        ${redAttr}, 
+                        ${greenAttr}, 
+                        ${blueAttr}, ${opacity})`;
+                        return
+                }
+
+                e.target.style.backgroundColor = `rgba(
+                    ${redAttr}, 
+                    ${greenAttr}, 
+                    ${blueAttr}, ${opacity})`;
+
+                e.target.classList.add('hovered');
+
+            })
+        });
     }
 
     // Prompt for new dimensions with validation, call reset, and call createGrid
@@ -40,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Add click event for prompt
     button.addEventListener("click", promptForSquares);
 
-    // Reset grid when called
+    // Reset grid to empty state
     resetSketch = () => {
         const allRows = Array.from(document.getElementsByClassName('row'));
         const allSquares = Array.from(document.getElementsByClassName('square'));
